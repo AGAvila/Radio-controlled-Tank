783b8c388fb2415c83bab735235d8ab9ddae5ce9
AGAvila <alvarogavila00@gmail.com>
2023-12-29 18:19:02 +0100

    Fixed calculation of the PWM of the motors

--- Code/Radio_Controller_Tank/motorsControl.py
+++ Code/Radio_Controller_Tank/motorsControl.py
@@ -1,6 +1,8 @@
+from machine import Pin
+
 class DCMotor:
 
-    def __init__(self, pin1, pin2, pin3, pin4, enable_pin1, enable_pin2, min_duty=0, max_duty=65535):
+    def __init__(self, speed, pin1, pin2, pin3, pin4, enable_pin1, enable_pin2, min_duty=0, max_duty=65535):
         # Pines control side 1
         self.pin1 = pin1
         self.pin2 = pin2
@@ -13,13 +15,16 @@ class DCMotor:
         self.min_duty = min_duty
         self.max_duty = max_duty
 
-    def duty_cycle(self, speed):
+        self.speed = speed
+
+    def duty_cycle(self):
         """
         Calculation of the duty cycle from the percentage of speed.
 
         Returns:
         - None
         """
+
         if self.speed <= 0 or self.speed > 100:
             duty_cycle = 0
         else:
@@ -34,6 +39,7 @@ class DCMotor:
         Returns:
         - None
         """
+
         self.enable_pin1.duty_u16(0)
         self.enable_pin2.duty_u16(0)
         self.pin1.value(0)
@@ -41,22 +47,22 @@ class DCMotor:
         self.pin3.value(0)
         self.pin4.value(0)
 
-    def all_motors_forward(self, speed):
+    def all_motors_forward(self):
         """
         Turn ON all motors and make the chassis go forwards.
 
         Returns:
         - None
         """
-        self.speed = speed
-        self.enable_pin1.duty_u16(self.duty_cycle(self.speed))
-        self.enable_pin2.duty_u16(self.duty_cycle(self.speed))
+
+        self.enable_pin1.duty_u16(self.duty_cycle())
+        self.enable_pin2.duty_u16(self.duty_cycle())
         self.pin1.value(1)
         self.pin2.value(0)
         self.pin3.value(0)
         self.pin4.value(1)
 
-    def motor_backward(self, speed):
+    def motor_backward(self):
         """
         Turn ON all motors and make the chassis go backwards.
 
@@ -64,15 +70,14 @@ class DCMotor:
         - None
         """
 
-        self.speed = speed
-        self.enable_pin1.duty_u16(self.duty_cycle(self.speed))
-        self.enable_pin2.duty_u16(self.duty_cycle(self.speed))
+        self.enable_pin1.duty_u16(self.duty_cycle())
+        self.enable_pin2.duty_u16(self.duty_cycle())
         self.pin1.value(0)
         self.pin2.value(1)
         self.pin3.value(1)
         self.pin4.value(0)
 
-    def motor_1_forward(self, speed):
+    def motor_1_forward(self):
         """
         Turn ON motor 1. No change is made to the state of motor 2.
 
@@ -80,13 +85,12 @@ class DCMotor:
         - None
         """
 
-        self.speed = speed
-        self.enable_pin1.duty_u16(self.duty_cycle(self.speed))
+        self.enable_pin1.duty_u16(self.duty_cycle())
         self.enable_pin2.duty_u16(0)
         self.pin1.value(1)
         self.pin2.value(0)
 
-    def motor_2_forward(self, speed):
+    def motor_2_forward(self):
         """
         Turn ON motor 2. No change is made to the state of motor 1.
 
@@ -94,13 +98,12 @@ class DCMotor:
         - None
         """
 
-        self.speed = speed
         self.enable_pin1.duty_u16(0)
-        self.enable_pin2.duty_u16(self.duty_cycle(self.speed))
+        self.enable_pin2.duty_u16(self.duty_cycle())
         self.pin3.value(0)
         self.pin4.value(1)
 
-    def motor_rotation_clockwise(self, speed):
+    def motor_rotation_clockwise(self):
         """
         Turn ON motor 1 rotating backwards and motor 2 rotating forwards.
 
@@ -108,15 +111,14 @@ class DCMotor:
         - None
         """
 
-        self.speed = speed
-        self.enable_pin1.duty_u16(self.duty_cycle(self.speed))
-        self.enable_pin2.duty_u16(self.duty_cycle(self.speed))
+        self.enable_pin1.duty_u16(self.duty_cycle())
+        self.enable_pin2.duty_u16(self.duty_cycle())
         self.pin1.value(0)
         self.pin2.value(1)
         self.pin3.value(0)
         self.pin4.value(1)
 
-    def motor_rotation_anticlockwise(self, speed):
+    def motor_rotation_anticlockwise(self):
         """
         Turn ON motor 1 rotating forwards and motor 2 rotating backwards.
 
@@ -124,9 +126,8 @@ class DCMotor:
         - None
         """
 
-        self.speed = speed
-        self.enable_pin1.duty_u16(self.duty_cycle(self.speed))
-        self.enable_pin2.duty_u16(self.duty_cycle(self.speed))
+        self.enable_pin1.duty_u16(self.duty_cycle())
+        self.enable_pin2.duty_u16(self.duty_cycle())
         self.pin1.value(1)
         self.pin2.value(0)
         self.pin3.value(1)
